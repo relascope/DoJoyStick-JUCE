4f445af419fee4d63416a4fc477ed0154bc3f79b
Relascope DoJoy.at <guru@dojoy.at>
2026-01-27 15:51:31 +0100

    fix: correct MIDI note mapping for joystick buttons from 60+ to 36+ (better default for drums)

--- source/JoyStickMapper.cpp
+++ source/JoyStickMapper.cpp
@@ -5,7 +5,7 @@ JoyStickMapper::JoyStickMapper()
     // Map first 12 buttons to MIDI notes 60-71 (Middle C octave)
     for (int i = 0; i < 21; ++i)
     {
-        mappings.push_back({ i, 60 + i });
+        mappings.push_back({ i, 36 + i });
     }
 }
 
